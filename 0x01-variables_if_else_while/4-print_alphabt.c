bf7191bf46f01c912223bd5f4d9c8c93605f130b
AlbanWainaina <albanmwangi@gmail.com>
2022-10-17 13:06:09 -0700

    alphabet excluding

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		if (low != 'q' && low != 'e')
+			putchar(low);
+	}
+		purchar('\n');
+		return (0);
+}
