f099f8e590e1ac58b86447ed87c6baa6df3821c4
AlbanWainaina <albanmwangi@gmail.com>
2022-10-18 10:43:48 -0700

    lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet -code begins
+ *
+ * Return : 0
+ */
+
+void print_alphabet(void)
+{
+	for (char a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
