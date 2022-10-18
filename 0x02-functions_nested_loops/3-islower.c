8e6e2c1cf251895ffb1f3ad9bc97992559d26a72
AlbanWainaina <albanmwangi@gmail.com>
2022-10-18 11:34:46 -0700

    small boys

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - checks if
+ * number is lowercase
+ *
+ * Return: 1 if lowercase
+ */
+
+int _islower(int c)
+{
+	char a;
+
+	if (a >= 'a' && a <= 'z')
+		return (1);
+	else
+		return (0);
+}
