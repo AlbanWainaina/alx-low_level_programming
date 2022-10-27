b5e3af54d2469dbc260c647f12113e8012e0cc7d
AlbanWainaina <albanmwangi@gmail.com>
2022-10-27 10:39:10 -0700

    gfs

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,19 +9,19 @@
 
 char *_strcat(char *dest, char *src)
 {
-	char resultant = *dest;
+	char *resultant = dest;
 	int i, j;
 
-	while (dest[i] != '\0')
+	while (*dest[i] != '\0')
 	{
 		i++;
 	}
 
-	for (j = 0; src[j] != '\0'; j++)
+	for (j = 0; *src[j] != '\0'; j++)
 	{
-		dest[i] = src[j];
+		*dest[i] = *src[j];
 		i++;
 	}
-	dest[i] = '\0';
+	*dest[i] = '\0';
 	return (resultant);
 }
