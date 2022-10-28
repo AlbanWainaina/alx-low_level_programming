b5807e82a3358ad047b007709daa8c49edf53f0d
AlbanWainaina <albanmwangi@gmail.com>
2022-10-28 07:27:34 -0700

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - entry point of code
+ * @a: array
+ * @n: integer
+ * return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, c;
+
+	for (i = 0; (i < (n - 1) / 2); i++)
+	{
+		c = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = c;
+	}
+}
