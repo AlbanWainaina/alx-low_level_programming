40fcaf0526e972f3abc1e6f871ee2b4444267066
AlbanWainaina <albanmwangi@gmail.com>
2022-11-01 10:46:39 -0700

    changes

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _strchr - locates character
- * @s: the string
- * @c: character were trying to find
- * Return: pointer to occurence of character
+ * _strchr - locates a character in a string
+ * @c: occurrence of the character
+ * @s: in the string
+ * Return: a pointer to the first occurrence of the character
  */
 
 char *_strchr(char *s, char c)
