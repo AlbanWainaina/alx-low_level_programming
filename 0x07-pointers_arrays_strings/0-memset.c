184d4f828935a4c078b4f85cc0fa88400981eeda
AlbanWainaina <albanmwangi@gmail.com>
2022-11-01 08:25:53 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - fills first n bytes with character b
+ * @s: memory location to be added
+ * @b: character to add
+ * @n: bytes to be added
+ * Return: pointer to s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+	char *v = s;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = _putchar(b);
+	}
+	return (v);
+}
